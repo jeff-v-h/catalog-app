690887039f6c0dee49bbd311a86d9fcc5ac4ab30
vagrant <vagrant@vagrant.vm>
2018-04-28 21:51:55 +0000

    Add route for new item

--- app.py
+++ app.py
@@ -13,27 +13,32 @@ app = Flask(__name__)
 
 
 # Routes/pages
-# Main page showing the list of categories and latest items
+# Main page showing the list of categories and latest items (add item button if logged in)
 @app.route('/')
 def catalog():
 	return "This is the main page."
+
+# Page for adding an item (must be logged in)
+@app.route('/catalog/new/')
+def newItem():
+	return "This is a page to add an item."
 
 # Page for viewing all items within a category
 @app.route('/catalog/<string:category>/items/')
 def categoryList(category):
 	return "This is a category page."
 
-# Page for viewing a specific item
+# Page for viewing a specific item (with edit and delete items if logged in)
 @app.route('/catalog/<string:category>/<string:item>/')
 def itemInfo(category, item):
 	return "This is a page for an item."
 
-# Page to edit an item
+# Page to edit an item (must be logged in)
 @app.route('/catalog/<string:item>/edit/')
 def editItem(item):
 	return "This is the page to edit an item."
 
-# Page to confirming deletion of an item
+# Page to confirming deletion of an item (must be logged in)
 @app.route('/catalog/<string:item>/delete')
 def deleteItem(item):
 	return "This is the page to delete an item."
