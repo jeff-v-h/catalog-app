34c9a4697eee4659f70fcbe9df19031de6f5661f
vagrant <vagrant@vagrant.vm>
2018-04-29 13:27:51 +0000

    Create functionality to add new item to database

--- app.py
+++ app.py
@@ -16,32 +16,38 @@ app = Flask(__name__)
 # Main page showing the list of categories and latest items (add item button if logged in)
 @app.route('/', methods=['GET'])
 def catalog():
-	return "This is the main page."
+	return render_template('catalog.html')
 
 # Page for adding an item (must be logged in)
 @app.route('/catalog/new/', methods=['GET', 'POST'])
 def newItem():
-	return "This is a page to add an item."
+	if request.method == 'GET':
+		return render_template('newitem.html')
+	if request.method == 'POST':
+		newItem = Item(name = request.form['name'], category = request.form['category'], description = request.form['description'])
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for('itemInfo', category=newItem.category, item=newItem))
 
 # Page for viewing all items within a category
 @app.route('/catalog/<string:category>/items/', methods=['GET'])
 def categoryList(category):
-	return "This is a category page."
+	return render_template('categorylist.html')
 
 # Page for viewing a specific item (with edit and delete items if logged in)
 @app.route('/catalog/<string:category>/<string:item>/', methods=['GET'])
 def itemInfo(category, item):
-	return "This is a page for an item."
+	return render_template('iteminfo.html')
 
 # Page to edit an item (must be logged in)
 @app.route('/catalog/<string:item>/edit/', methods=['GET', 'PUT'])
 def editItem(item):
-	return "This is the page to edit an item."
+	return render_template('edititem.html')
 
 # Page to confirming deletion of an item (must be logged in)
 @app.route('/catalog/<string:item>/delete', methods=['GET', 'POST'])
 def deleteItem(item):
-	return "This is the page to delete an item."
+	return render_template('deleteitem.html')
 
 
 if __name__ == '__main__':
